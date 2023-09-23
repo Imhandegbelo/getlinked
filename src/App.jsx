fd99a45bb0bb956e12eadcc3bb84fbaa5227255e
Imhandegbelo <abramgeo5@gmail.com>
2023-09-23 22:31:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Contact from './pages/Contact'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="max-w-[1440px]">
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
