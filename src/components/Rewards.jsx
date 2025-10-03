7030cc4c55d32cc8ec6608fdbbcb93f4c7a858da
Imhandegbelo <abramgeo5@gmail.com>
2025-10-03 18:44:51 +0100

    cleanup

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -16,29 +16,27 @@ export default function Rewards() {
           key={prize.position}
           className={`text-center pb-4 border w-1/3  ${
             index === 1
-              ? "h-36 lg:h-[15rem] xl:h-[19rem] border-[#903AFF] bg-[#903AFF]/10"
-              : "h-36 lg:h-[14rem] xl:h-[18rem] border-[#D434FE] bg-[#D434FE]/10"
-          } rounded-lg lg:px- xl:px-6 relative pt-[5rem]`}
+              ? "border-[#903AFF] bg-[#903AFF]/10"
+              : "border-[#D434FE] bg-[#D434FE]/10"
+          } rounded-lg`}
         >
           <img
             src={prize.image}
             alt={`medal-${index + 1}`}
-            className={`absolute  ${
+            className={`${
               index === 1
-                ? "max-w-[7rem] lg:max-w-[12rem] xl:max-w-[16rem] -left-2 lg:-left-8 -top-[40%]"
-                : "-top-[30%] lg:left-1"
+                ? "md:scale-[135%] mx-auto"
+                : ""
             }`}
           />
           <p
-            className={`font-bold text-xs lg:text-4xl ${
-              index === 1 ? "lg:mt-9 xl:mt-24" : "lg:mt-8 xl:mt-20"
-            }`}
+            className="font-bold text-xs lg:text-4xl mt-8"
           >
             {prize.position}
           </p>
           <p className="text-xs lg:text-2xl font-semibold">Runner</p>
           <p
-            className={`text-sm lg:text-2xl xl:text-[2rem] font-bold ${
+            className={`text-sm lg:text-[160%] leading-10 font-bold ${
               index === 1 ? "text-[#903AFF]" : "text-[#D434FE]"
             }`}
           >
